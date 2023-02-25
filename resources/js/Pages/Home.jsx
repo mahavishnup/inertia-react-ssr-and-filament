3eff77f036bb9d25e487a6ff3c7eb9edc8ad1901
mahavishnup <selvamvishnu25@gmail.com>
2023-02-25 22:46:29 +0530

    init

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import RootLayout from "@/Layouts/RootLayout";
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return (
+        <RootLayout>
+            <Head>
+                <title>Home</title>
+            </Head>
+
+            <h2>Home</h2>
+        </RootLayout>
+    );
+}
